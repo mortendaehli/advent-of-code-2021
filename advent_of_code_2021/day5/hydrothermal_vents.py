67c1ed5369a9959576ad4e5431c6a64606982077
morten.aslesen <mortendaehli@gmail.com>
2021-12-16 19:50:20 +0100

    feat: Complete day 5

--- advent_of_code_2021/day5/hydrothermal_vents.py
+++ advent_of_code_2021/day5/hydrothermal_vents.py
@@ -1,28 +1,46 @@
 from typing import Tuple, List
+from collections import Counter
 
 from dataclasses import dataclass
 
 
 @dataclass
-class Point:
-    x: int
-    y: int
-
-
-@dataclass
 class Line:
-    p1: Point
-    p2: Point
+    p1: Tuple[int, int]
+    p2: Tuple[int, int]
+
+    @property
+    def is_horizontal(self) -> bool:
+        return (self.p1[0] != self.p2[0]) & (self.p1[1] == self.p2[1])
+
+    @property
+    def is_vertical(self) -> bool:
+        return (self.p1[1] != self.p2[1]) & (self.p1[0] == self.p2[0])
+
+    @property
+    def is_diagonal(self) -> bool:
+        return abs(self.p1[0] - self.p2[0]) == abs(self.p1[1] - self.p2[1])
 
 
 def read_data() -> List[Line]:
     with open("data.txt", "r") as file:
         data = file.readlines()
-    return [Line(*[Point(*point.split(",")) for point in line.strip().split("->")]) for line in data]
+    return [Line(*[tuple([int(i) for i in point.split(",")]) for point in line.strip().split("->")]) for line in data]
 
 
-def calculate_points_in_line(line: Line) -> List[Point]:
-    pass
+def calculate_points_in_line(line: Line, include_diagonal: bool) -> List[Tuple[int, int]]:
+
+    (x1, y1), (x2, y2) = sorted([line.p1, line.p2])
+
+    if line.is_horizontal or line.is_vertical:
+        return [(x, y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)]
+    elif line.is_diagonal & include_diagonal:
+        if y1 < y2:
+            return [(x, y1 + i) for i, x in enumerate(range(x1, x2 + 1))]
+        else:
+            return [(x, y1 - i) for i, x in enumerate(range(x1, x2 + 1))]
+    else:
+        return []
 
 
 def part_one() -> None:
@@ -32,13 +50,27 @@ def part_one() -> None:
     """
     print("Part 1: At how many points do at least two lines overlap?")
     lines = read_data()
+    point_list = list()
     for line in lines:
-        calculate_points_in_line(line=line)
-    print("Yes!")
+        point_list += calculate_points_in_line(line=line, include_diagonal=False)
+
+    print(f"Number of overlapping points: ")
+    print(sum([1 for x in Counter(point_list).items() if x[1] >= 2]))
 
 
 def part_two() -> None:
-    pass
+    """
+    Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture;
+    you need to also consider diagonal lines.
+    """
+    print("Part 1: At how many points do at least two lines overlap?")
+    lines = read_data()
+    point_list = list()
+    for line in lines:
+        point_list += calculate_points_in_line(line=line, include_diagonal=True)
+
+    print(f"Number of overlapping points including diagonals: ")
+    print(len([i for i in Counter(point_list).values() if i > 1]))
 
 
 if __name__ == "__main__":
