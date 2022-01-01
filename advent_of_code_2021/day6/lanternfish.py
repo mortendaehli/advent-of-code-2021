34d60fded83009bb12ab3a3753c3c3cf35b9b1ae
morten.aslesen <mortendaehli@gmail.com>
2022-01-01 17:23:49 +0100

    refactor: Day 6

--- advent_of_code_2021/day6/lanternfish.py
+++ advent_of_code_2021/day6/lanternfish.py
@@ -8,7 +8,7 @@ def read_fish() -> List[int]:
 
 
 def get_next_states(current_states: Dict[int, int]) -> Dict[int, int]:
-    next_states = {i: current_states[i+1 if i < 8 else 0]for i in range(9)}
+    next_states = {i: current_states[i + 1 if i < 8 else 0] for i in range(9)}
 
     # Reset adult fish to state 6.
     next_states[6] += current_states[0]
@@ -20,13 +20,7 @@ def get_initial_state(starting_fish: List[int]) -> Dict[int, int]:
     return {i: starting_fish.count(i) for i in range(9)}
 
 
-def part_one() -> None:
-    """
-    Find a way to simulate lanternfish.
-
-    How many lanternfish would there be after 80 days? 379114
-    """
-    print("Part 1: Find a way to simulate lanternfish.")
+def part_one() -> int:
     initial_state = get_initial_state(starting_fish=read_fish())
 
     current_state = initial_state
@@ -34,14 +28,10 @@ def part_one() -> None:
         current_state = get_next_states(current_states=current_state)
     final_count = sum([x for x in current_state.values()])
 
-    print(f"How many lanternfish would there be after 80 days?: {final_count}")
+    return final_count
 
 
-def part_two() -> None:
-    """
-    Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
-    """
-    print("Part 2: Find a way to simulate lanternfish.")
+def part_two() -> int:
     initial_state = get_initial_state(starting_fish=read_fish())
 
     current_state = initial_state
@@ -49,10 +39,15 @@ def part_two() -> None:
         current_state = get_next_states(current_states=current_state)
     final_count = sum([x for x in current_state.values()])
 
-    print(f"How many lanternfish would there be after 256 days?: {final_count}")
+    return final_count
 
 
 if __name__ == "__main__":
-    part_one()
-    part_two()
-
+    print("Day 6: Lanternfish")
+    print("-" * 80)
+    result_part_1 = part_one()
+    print(f"Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?: {result_part_1}")
+    print("-" * 80)
+    result_part_2 = part_two()
+    print(f"Part 2: How many lanternfish would there be after 256 days?: {result_part_2}")
+    print("-" * 80)
