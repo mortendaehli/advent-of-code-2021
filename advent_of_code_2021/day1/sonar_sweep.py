50295aa1b8bcbeb986693a5a0033c6c47faa77db
morten.aslesen <mortendaehli@gmail.com>
2022-01-01 17:16:52 +0100

    refactor: Day 1

--- advent_of_code_2021/day1/sonar_sweep.py
+++ advent_of_code_2021/day1/sonar_sweep.py
@@ -2,9 +2,6 @@ from typing import List
 
 
 def load_array() -> List[float]:
-    """
-    Loading the input data and converting each line into int.
-    """
     with open("data.txt", "r") as file:
         data = file.read().splitlines()
     return [float(x) for x in data]
@@ -20,26 +17,26 @@ def part_one() -> float:
     return calculate_measurements_larger_than_previous(array=array)
 
 
-
 def part_two() -> float:
     sliding_window = 3
     array = load_array()
 
     # Need to consider the sum before index == sliding_window in a separate step in order to avoid wasted computation
     pre_sliding_array = [sum(array[:x]) / x for x in range(2, sliding_window)]
+
     sliding_array = pre_sliding_array + list(
-        map(lambda i: sum(array[i-sliding_window: i]) / sliding_window, range(sliding_window, len(array)))
+        map(lambda i: sum(array[i - sliding_window : i]) / sliding_window, range(sliding_window, len(array)))
     )
 
     return calculate_measurements_larger_than_previous(array=sliding_array)
 
 
 if __name__ == "__main__":
-    """
-    --- Day 1: Sonar Sweep ---
-    """
+    print("Day 1: Sonar Sweep")
+    print("-" * 80)
     result_part_1 = part_one()
     print(f"Part 1: How many measurements are larger than the previous measurement?: {result_part_1}")
-
+    print("-" * 80)
     result_part_2 = part_two()
     print(f"Part 2: How many sums are larger than the previous sum?: {result_part_2}")
+    print("-" * 80)
